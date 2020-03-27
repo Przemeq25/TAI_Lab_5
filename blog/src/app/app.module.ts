3bc07b7dfedfb26ed38db90306e78b16826e8e51
Przemysław Cichoń <przemekcichon97@gmail.com>
2020-03-27 21:42:34 +0100

    added routing and navbar component

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactComponent } from './components/contact/contact.component';
     
     HomeComponent,
     BlogComponent,
-    ContactComponent
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
