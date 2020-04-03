335482d0c51d8d646699f2b6c11e4e94e4fc7a08
Przemysław Cichoń <przemekcichon97@gmail.com>
2020-04-03 14:17:09 +0200

    added blog components

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -2,24 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { BlogComponent } from './components/blog/blog.component';
+
+import {BlogItemDetailsComponent} from './components/blog-item-details/blog-item-details.component';
+import {BlogHomeComponent} from './components/blog-home/blog-home.component';
 
 
 const routes: Routes = [
   {
-    path:'home',
-    component:HomeComponent,
-    
+    path: 'home',
+    component: HomeComponent,
   },
   {
-    path:'contact',
-    component:ContactComponent
+    path: 'contact',
+    component: ContactComponent
   },
   {
-    path:'blog',
-    component:BlogComponent
+    path: 'blog',
+    component: BlogHomeComponent
   },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  {
+    path: '**', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent
+  },
+
 
 ];
 
